a3417d30c796add35d7339cef5cf3efb84038c56
Justin Leveck <jleveck@gmail.com>
2013-10-04 16:21:59 +0000

    Tests pass

--- subclasses_test.rb
+++ subclasses_test.rb
@@ -37,9 +37,19 @@ class SubClassesTest < Minitest::Test
       position == :aisle
     end
 
+    def middle_seat?
+      position == :middle
+    end
+
     def legroom?
       false
     end
+  end
+
+  class CoachSeat < AirlineSeat
+  end
+
+  class BusinessClassSeat < AirlineSeat
   end
 
   #
@@ -52,44 +62,35 @@ class SubClassesTest < Minitest::Test
   end
 
   def test_airline_seat_has_instance_method_aisle?
-    skip
-    # Check to see if an AirlineSeat has method :aisle?
+    assert AirlineSeat.instance_methods.include?(:aisle?)
   end
 
   def test_airline_seat_has_instance_method_middle_seat?
-    skip
-    # Check to see if an AirlineSeat has method :middle_seat?
-    # Create the method middle_seat?
+    assert AirlineSeat.instance_methods.include?(:middle_seat?)
   end
 
   def test_coach_seat_has_instance_method_window?
-    skip
     assert CoachSeat.instance_methods.include?(:window?)
   end
 
   def test_coach_seat_has_instance_method_aisle?
-    skip
-    # TODO: Write the test to ensure CoachSeat has an instance method `aisle?`
+    assert CoachSeat.instance_methods.include?(:aisle?)
   end
 
   def test_coach_seat_has_instance_method_middle_seat?
-    skip
-    # TODO: Write the test to ensure CoachSeat has an instance method `middle_seat?`
+    assert CoachSeat.instance_methods.include?(:middle_seat?)
   end
 
   def test_business_class_seat_has_instance_method_window?
-    skip
     assert BusinessClassSeat.instance_methods.include?(:window?)
   end
 
   def test_business_class_seat_has_instance_method_aisle?
-    skip
-    # TODO: Write the test to ensure BusinessClassSeat has an instance method `aisle?`
+    assert BusinessClassSeat.instance_methods.include?(:aisle?)
   end
 
   def test_business_class_seat_has_instance_method_middle_seat?
-    skip
-    # TODO: Write the test to ensure BusinessClassSeat has an instance method `middle_seat?`
+    assert BusinessClassSeat.instance_methods.include?(:middle_seat?)
   end
 
 end
